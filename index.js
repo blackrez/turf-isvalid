4f1c8e38a5bb218e186e2b2b77a20eb7e5fd914c
Nabil Servais <nabil@eml.cc>
2015-12-04 14:52:41 +0100

    Initial commit extrated code from https://github.com/blackrez/apicarto-qpv/blob/master/helpers/isvalidgeo.js

---
+++ index.js
@@ -0,0 +1,34 @@
+// depend on jsts for now https://github.com/bjornharrtell/jsts/blob/master/examples/overlay.html
+var jsts = require('jsts');
+
+/*@module turf/valid
+ * @category Helper
+ * @param {Feature<>} Feature, it can be point, string or polygon
+ * @return {Boolean}
+ *
+ * @example
+ * var poly = {
+ *   "type": "Feature",
+ *   "properties": {
+ *     "fill": "#0f0"
+ *   },
+ *   "geometry": {
+ *     "type": "Polygon",
+ *     "coordinates": [[
+ *       [-122.801742, 45.48565],
+ *     ]]
+ *   }
+ * }
+ *
+ * > var isvald = turf.valid(poly);
+ * > console.log(isvald)
+ * true
+ */
+ 
+module.exports = function(geom) {
+
+  var reader = new jsts.io.GeoJSONReader();
+  var g = reader.read(JSON.stringify(geom));
+
+  return g.geometry.isValid()
+};
